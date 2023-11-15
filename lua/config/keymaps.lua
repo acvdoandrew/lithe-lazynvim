c41907df44b8d3bbaef806d962843ed6769c5e78
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2023-11-14 20:10:53 -0500

    configures cowboy

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local discipline = require("acvdoandrew.discipline")
+discipline.cowboy()
+
 local map = vim.keymap.set
 
 -- vim-tmux-navigator
